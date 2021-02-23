92efdd1e498d0dbcca722fd3d7d357dfce945709
Nicholas V Frangione <nfrangione8@gmail.com>
2021-02-23 12:25:53 -0800

    commit tree 2a855ddb03595e242d80534975fd1f21d4b55c0d

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+var upperCaseCustomerName = () => {
+    customerName = customerName.toUpperCase()
+    return customerName;
+}
+var setBestCustomer = () => {
+    bestCustomer = 'not bob';
+    return bestCustomer;
+}
+
+var overwriteBestCustomer = () => {
+    bestCustomer = 'maybe bob'
+    return bestCustomer;
+}
+
+const leastFavoriteCustomer = '';
+
+var changeLeastFavoriteCustomer = () => {
+    leastFavoriteCustomer = 'bob'
+}
